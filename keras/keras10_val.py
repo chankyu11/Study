244c876efa067131778e34853df0ac88efb1d377
chankyu11 <lck1130@naver.com>
2020-05-14 20:16:12 +0900

    20200514

--- keras/keras10_val.py
+++ keras/keras10_val.py
@@ -1,8 +1,3 @@
-# 과제: R2를 음수가 아닌 0.5이하로 줄이기
-# 레이어는 인풋과 아웃풋을 포함 5개 이상, 노드는 레이어당 각각 5개 이상.
-# batch_size = 1
-# epochs = 100 이상.
-
 # 1. 데이터
 import numpy as np
 
@@ -12,24 +7,48 @@ x_train = np.array([1,2,3,4,5,6,7,8,9,10])
 y_train = np.array([1,2,3,4,5,6,7,8,9,10])
 x_test = np.array([11,12,13,14,15])
 y_test = np.array([11,12,13,14,15])
-x_pred = np.array([16,17,18]) # pred = predict 
+x_val = np.array([101,102,103,104,105,106])
+y_val = np.array([101,102,103,104,105,106])
+# x_pred = np.array([16,17,18]) # pred = predict
+
 
 # 2. 모델구성
 from keras.models import Sequential
 from keras.layers import Dense
 
 model = Sequential()
-model.add(Dense(1, input_dim = 1))
-model.add(Dense(1000))
-model.add(Dense(1000))
-model.add(Dense(100))
+model.add(Dense(5, input_dim = 1))
+model.add(Dense(10))
+model.add(Dense(10))
+model.add(Dense(10))
+model.add(Dense(10))
+model.add(Dense(10))
+model.add(Dense(10))
+model.add(Dense(10))
+model.add(Dense(10))
+model.add(Dense(10))
+model.add(Dense(10))
+model.add(Dense(10))
+model.add(Dense(10))
+model.add(Dense(10))
 # model.add(Dense(10))
-model.add(Dense(100))
+# model.add(Dense(10))
+# model.add(Dense(10))
+# model.add(Dense(10))
+# model.add(Dense(10))
+# model.add(Dense(10))
+# model.add(Dense(10))
+# model.add(Dense(10))
+# model.add(Dense(10))
+# model.add(Dense(10))
+model.add(Dense(7))
+model.add(Dense(10))
+model.add(Dense(5))
 model.add(Dense(1))
 
 # 3. 훈련
 model.compile(loss = 'mse', optimizer = 'adam', metrics = ['mse'])
-model.fit(x_train, y_train, epochs = 700, batch_size = 1)
+model.fit(x_train, y_train, epochs = 500, batch_size = 1, validation_data = (x_val, y_val))
 
 
 # 4. 평가와 예측
--- keras/keras11_split.py
+++ keras/keras11_split.py
@@ -1,35 +1,36 @@
-# 과제: R2를 음수가 아닌 0.5이하로 줄이기
-# 레이어는 인풋과 아웃풋을 포함 5개 이상, 노드는 레이어당 각각 5개 이상.
-# batch_size = 1
-# epochs = 100 이상.
-
 # 1. 데이터
 import numpy as np
 
-# x = np.array([1,2,3,4,5,6,7,8,9,10])
-# y = np.array([1,2,3,4,5,6,7,8,9,10])
-x_train = np.array([1,2,3,4,5,6,7,8,9,10])
-y_train = np.array([1,2,3,4,5,6,7,8,9,10])
-x_test = np.array([11,12,13,14,15])
-y_test = np.array([11,12,13,14,15])
-x_pred = np.array([16,17,18]) # pred = predict 
+x = np.array(range(1,101))
+y = np.array(range(101,201))
+
+x_train = x[:60]
+x_val = x[60:80]
+x_test = x[80:]
+
+y_train = x[:60]
+y_val = x[60:80]
+y_test = x[80:]
 
 # 2. 모델구성
 from keras.models import Sequential
 from keras.layers import Dense
 
 model = Sequential()
-model.add(Dense(1, input_dim = 1))
-model.add(Dense(1000))
-model.add(Dense(1000))
-model.add(Dense(100))
-# model.add(Dense(10))
-model.add(Dense(100))
+model.add(Dense(5, input_dim = 1))
+model.add(Dense(10))
+model.add(Dense(10))
+model.add(Dense(10))
+model.add(Dense(10))
+model.add(Dense(10))
+model.add(Dense(7))
+model.add(Dense(10))
+model.add(Dense(5))
 model.add(Dense(1))
 
 # 3. 훈련
 model.compile(loss = 'mse', optimizer = 'adam', metrics = ['mse'])
-model.fit(x_train, y_train, epochs = 700, batch_size = 1)
+model.fit(x_train, y_train, epochs = 30, batch_size = 1, validation_data = (x_val, y_val))
 
 
 # 4. 평가와 예측
@@ -39,7 +40,7 @@ loss, mse = model.evaluate(x_test, y_test, batch_size = 1)
 print("loss: ", loss)
 print("mse:", mse)
 
-0# y_pred = model.predict(x_pred)
+# y_pred = model.predict(x_pred)
 # print("y_pred:", y_pred)
 
 y_predict = model.predict(x_test)
