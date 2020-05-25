b84bf930da00c68a1e612bfcd11a8b0f0e916de7
chankyu11 <lck1130@naver.com>
2020-05-25 20:53:03 +0900

    split

---
+++ keras/keras40_lstm_split1.py
@@ -0,0 +1,68 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Input, LSTM
+
+# 1. 데이터
+
+a = np.array(range(1,11))
+size = 5    # time steps = 4
+
+# lstm 모델 완성.
+
+def split_x(seq, size):
+    aaa = []
+    for i in range(len(seq) - size + 1):
+        subset = seq[i : (i + size)]
+        aaa.append([item for item in subset])
+    # print(type(aaa))
+    return np.array(aaa)
+
+dataset = split_x(a, size)
+print("=========================================")
+print(dataset)
+
+print(dataset.shape)
+print(type(dataset))
+
+x = dataset[:,0:4]
+# 0~4 즉 1,2,3,4 를 가져오겠다는 이야기
+y = dataset[:,4]
+#  4열을 즉 마지막 자리만 가져오겠다.
+print(x)
+print(y)
+
+print(x.shape)
+print(y.shape)
+
+x = x.reshape(x.shape[0],x.shape[1],1)
+# x = np.reshape(x,(6,4,1)) 위와 같은 문법
+
+# 2. 모델
+
+model = Sequential()
+model.add(LSTM(10, input_shape = 4, input_dim = 1))
+model.add(Dense(15))
+model.add(Dense(25))
+model.add(Dense(35))
+model.add(Dense(25))
+model.add(Dense(15))
+model.add(Dense(10))
+model.add(Dense(5))
+model.add(Dense(1))
+
+model.summary
+from keras.callbacks import EarlyStopping
+
+es = EarlyStopping(monitor='loss', patience= 10, mode = 'auto')
+
+model.compile(loss = 'mse', optimizer='adam', metrics=['mse'])
+model.fit(x,y,epochs=30,batch_size=1, verbose=1, callbacks = [es])
+
+loss, mse = model.evaluate(x, y)
+
+y_predict = model.predict(x)
+print(y_predict)
+
+print('loss', loss)
+print('mse', mse)
+
