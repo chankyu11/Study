4417772f89ac45c7d6e728f43542ed21ac142331
chankyu11 <lck1130@naver.com>
2020-07-03 15:24:03 +0900

    vgg16

---
+++ keras/100 ~/keras116_vgg16.py
@@ -0,0 +1,22 @@
+from keras.applications import VGG16,VGG19, Xception, ResNet101,ResNet101V2,ResNet152
+from keras.applications import ResNet152V2,ResNet50,ResNet50V2,InceptionV3,InceptionResNetV2
+from keras.applications import MobileNet,MobileNetV2,DenseNet121,DenseNet169,DenseNet201
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, MaxPooling2D, BatchNormalization, Activation,Flatten
+from keras.optimizers import Adam
+
+# vgg16 = VGG16()
+# take_model = VGG19()
+applications = [VGG19, Xception, ResNet101, ResNet101V2, ResNet152,ResNet152V2, ResNet50, 
+                ResNet50V2, InceptionV3, InceptionResNetV2,MobileNet, MobileNetV2, 
+                DenseNet121, DenseNet169, DenseNet201]
+
+for i in applications:
+    take_model = i()
+# model = Sequential()
+# model.add(vgg16)
+# model.add(Flatten())
+# model.add(Dense(256))
+# model.add(BatchNormalization())
+# model.add(Activation('relu'))
+# model.add(Dense(10, activation='softmax'))
