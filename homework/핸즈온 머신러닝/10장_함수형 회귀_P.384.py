06c82b177710d755f72fc7f8ea75af5442bca556
chankyu11 <lck1130@naver.com>
2020-07-05 22:48:15 +0900

    핸즈온 머신러닝

---
+++ homework/핸즈온 머신러닝/10장_함수형 회귀_P.384.py
@@ -0,0 +1,34 @@
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from tensorflow import keras
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+# 1. 데이터
+housing = fetch_california_housing()
+
+# train, test 분리
+X_train_full, X_test, y_train_full, y_test = train_test_split(housing.data, housing.target, random_state=42)
+X_train, X_valid, y_train, y_valid = train_test_split(X_train_full, y_train_full, random_state=42)
+
+# 표준화
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_valid = scaler.transform(X_valid)
+X_test = scaler.transform(X_test)
+
+# 2. 모델
+input_ = keras.layers.Input(shape=X_train.shape[1:])
+hidden1 = keras.layers.Dense(30, activation="relu")(input_)
+hidden2 = keras.layers.Dense(30, activation="relu")(hidden1)
+concat = keras.layers.concatenate([input_, hidden2])
+output = keras.layers.Dense(1)(concat)
+model = keras.models.Model(inputs=[input_], outputs=[output])
+
+model.compile(loss="mean_squared_error", optimizer=keras.optimizers.SGD(lr=1e-3)) # 1e-3 0.0001
+history = model.fit(X_train, y_train, epochs=20, validation_data=(X_valid, y_valid))
+mse_test = model.evaluate(X_test, y_test)
+X_new = X_test[:3]
+y_pred = model.predict(X_new)
