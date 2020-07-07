a29c8ceeb658d5a80f4b935539e6ad3720551d12
chankyu11 <lck1130@naver.com>
2020-07-07 18:44:44 +0900

    TF_Linear

---
+++ tf/tf05_Linear.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+
+tf.set_random_seed(777)
+
+x_train = [1,2,3]
+y_train = [1,2,3]
+
+W = tf.Variable(tf.random_normal([1]), name = 'weight')
+b = tf.Variable(tf.random_normal([1], name = 'bias'))
+
+sess = tf.Session()
+
+sess.run(tf.global_variables_initializer())
+# 초기화 변수는 초기화 후 진행해야함
+
+print(sess.run(W))
+
+hypothesis = x_train * W + b
+
+cost = tf.reduce_mean(tf.square(hypothesis - y_train))
+
+train = tf.train.GradientDescentOptimizer(learning_rate = 0.01).minimize(cost)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+
+    for step in range(2001):
+        _, cost_val, W_val, b_val = sess.run([train, cost, W, b])
+
+        if step % 20 ==0:
+            print(step, cost_val, W_val, b_val)
