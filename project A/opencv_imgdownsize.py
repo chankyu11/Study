101fd5b39cd2c76ce8bdf64a4d4a6a788278a8d4
chankyu11 <lck1130@naver.com>
2020-08-31 00:44:08 +0900

    Project A(watcher)

---
+++ project A/opencv_imgdownsize.py
@@ -0,0 +1,16 @@
+import cv2
+
+src = cv2.imread("C:/Users/bitcamp/Desktop/1.jpg", cv2.IMREAD_COLOR)
+
+h, w, c = src.shape
+
+# 사이즈 확대
+dst = cv2.pyrUp(src, dstsize=(w*2, h*2), borderType=cv2.BORDER_DEFAULT);
+
+# 사이즈 축소
+dst = cv2.pyrDown(src)
+
+cv2.imshow("src", src)
+cv2.imshow("dst", dst)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
