5038b83e6c3157a22141ff464127f970dd269d60
chankyu11 <lck1130@naver.com>
2020-08-31 01:22:45 +0900

    SQL

--- fast_cam/SQL/dump.sql
+++ fast_cam/SQL/dump.sql
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 BEGIN TRANSACTION;
 CREATE TABLE users(id INTEGER PRIMARY KEY, username text, email text, phone text, website text, regdate text);
 INSERT INTO "users" VALUES(1,'kim','lee@naver.com','010-0012-1212','lee.com','2020-08-13  15:08:17');
@@ -6,3 +7,13 @@ INSERT INTO "users" VALUES(3,'lee','lee@naver.com','010-1233-1234','lee.com','20
 INSERT INTO "users" VALUES(4,'cho','cho@naver.com','010-1236-1234','cho.com','2020-08-13  15:08:17');
 INSERT INTO "users" VALUES(5,'kwon','kwon@naver.com','010-1239-1234','kwon.com','2020-08-13  15:08:17');
 COMMIT;
+=======
+BEGIN TRANSACTION;
+CREATE TABLE users(id INTEGER PRIMARY KEY, username text, email text, phone text, website text, regdate text);
+INSERT INTO "users" VALUES(1,'kim','lee@naver.com','010-0012-1212','lee.com','2020-08-13  15:08:17');
+INSERT INTO "users" VALUES(2,'lee','park@naver.com','010-2332-2332','google.com','2020-08-13  15:08:17');
+INSERT INTO "users" VALUES(3,'lee','lee@naver.com','010-1233-1234','lee.com','2020-08-13  15:08:17');
+INSERT INTO "users" VALUES(4,'cho','cho@naver.com','010-1236-1234','cho.com','2020-08-13  15:08:17');
+INSERT INTO "users" VALUES(5,'kwon','kwon@naver.com','010-1239-1234','kwon.com','2020-08-13  15:08:17');
+COMMIT;
+>>>>>>> 667c42ee521f20fb0ad8f218b4ec214b25aaf949
--- fast_cam/SQL/section12_2.py
+++ fast_cam/SQL/section12_2.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # Section12-2
 # 파이썬 데이터 베이스 연동(SQLite)
 # 테이블 조회
@@ -89,4 +90,97 @@ with conn:
         print('Dump print complete')
 
 # f.close(), conn.close() 가 with문 때문에 자동으로 생성 
+=======
+# Section12-2
+# 파이썬 데이터 베이스 연동(SQLite)
+# 테이블 조회
+
+import sqlite3
+
+# DB파일 조회(없으면 새로 생성)
+
+conn = sqlite3.connect('D:/STUDY/fast_cam/SQL/database.db') # 본인 DB 경로
+
+# 커서 바인딩
+
+c = conn.cursor()
+
+# 데이터 조회(전체)
+c.execute("SELECT * FROM users")
+
+# 커서 위치 변경
+# 1개 로우 선택
+
+# print('one -> \n', c.fetchone())
+
+# 지정 로우 선택
+# print('Three -> \n', c.fetchmany(3))
+
+# 전체 로우 선택
+# print('ALL -> \n', c.fetchall())
+
+# 커서가 위치를 기억하고 있다. 그래서 하나 뽑아오고 세개 뽑을대 2~4까지 출력되고 다 뽑으라니까 '5'만 나온거
+
+print("===" * 30)
+
+# 순회1
+
+# rows = c.fetchall()
+# for row in rows:
+#     print('retrieve1 >', row)
+
+# 순회2
+# for row in c.fetchall():
+#     print('retrieve2 >', row)
+
+# # 순회3
+# for row in c.execute('SELECT * FROM users ORDER BY id desc'):
+#     print('retrieve3 >', row)
+# 코드를 이렇게 작성하면 c.execute를 따로 안해줘도 됨
+print("===" * 30)
+
+# WHERE Retrieve1
+
+param1 = (3, )
+c.execute('SELECT * FROM users WHERE id = ?', param1)
+print('param1', c.fetchone())
+print('param1', c.fetchall())
+
+# WHERE Retrieve2
+
+param2 = 4
+c.execute('SELECT * FROM users WHERE id = "%s"' %param2) # %s 문자 %f소수 %d정수
+print('param2', c.fetchone())
+print('param2', c.fetchall())
+
+# WHERE Retrieve3
+
+c.execute('SELECT * FROM users WHERE id = :Id' ,{"Id" : 5}) # %s 문자 %f소수 %d정수
+print('param3', c.fetchone())
+print('param3', c.fetchall())
+
+# WHERE Retrieve4
+
+param4 = (3,5)
+c.execute('SELECT * FROM users WHERE id IN(?,?)', param4)
+print('param4:', c.fetchall())
+# 위와 방식은 비슷하게 따로 param선언 no
+c.execute("SELECT * FROM users WHERE id IN('%d','%d')"%(3,4))
+print('param4:', c.fetchall())
+
+# WHERE Retrieve4
+# or 연산자로 조회
+c.execute("SELECT * FROM users WHERE id= :id1 OR id=:id2",{"id1":2, "id2":5})
+print('param4:', c.fetchall())
+
+# dump 출력
+
+with conn:
+    with open("D:/STUDY/fast_cam/SQL/dump.sql",'w') as f:
+        for line in conn.iterdump():
+            f.write('%s\n' % line)
+        print('Dump print complete')
+
+# f.close(), conn.close() 가 with문 때문에 자동으로 생성 
+>>>>>>> 667c42ee521f20fb0ad8f218b4ec214b25aaf949
 # dump를 통해 
