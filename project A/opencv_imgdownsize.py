33b8df6e0b84aa18325f5c8967aa0770bd7826cc
chankyu11 <lck1130@naver.com>
2020-09-12 21:33:44 +0900

    A

--- project A/opencv_imgdownsize.py
+++ project A/opencv_imgdownsize.py
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
 import cv2
 
-src = cv2.imread("C:/Users/bitcamp/Desktop/1.jpg", cv2.IMREAD_COLOR)
+
+src = cv2.imread("D:/googleImageDownloader-master/images/person/0 (1).jpg", cv2.IMREAD_COLOR)
 
 h, w, c = src.shape
 
@@ -14,21 +14,4 @@ dst = cv2.pyrDown(src)
 cv2.imshow("src", src)
 cv2.imshow("dst", dst)
 cv2.waitKey(0)
-=======
-import cv2
-
-src = cv2.imread("C:/Users/bitcamp/Desktop/1.jpg", cv2.IMREAD_COLOR)
-
-h, w, c = src.shape
-
-# 사이즈 확대
-dst = cv2.pyrUp(src, dstsize=(w*2, h*2), borderType=cv2.BORDER_DEFAULT);
-
-# 사이즈 축소
-dst = cv2.pyrDown(src)
-
-cv2.imshow("src", src)
-cv2.imshow("dst", dst)
-cv2.waitKey(0)
->>>>>>> 667c42ee521f20fb0ad8f218b4ec214b25aaf949
 cv2.destroyAllWindows()
