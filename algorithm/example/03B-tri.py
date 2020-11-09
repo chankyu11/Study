cf32777f74d83c9ad2040d1ae94f558881e2c833
CHANKYU\lck11 <lck1130@naver.com>
2020-11-09 13:17:10 +0900

    .

---
+++ algorithm/example/03B-tri.py
@@ -0,0 +1,20 @@
+import turtle as t
+
+d = 100
+
+t.fd(d)
+t.lt(120)
+t.fd(d)
+t.lt(120)
+t.fd(d)
+t.lt(120)
+
+t.fd(100)
+t.lt(90)
+t.fd(100)
+t.lt(90)
+t.fd(100)
+t.lt(90)
+t.fd(100)
+
+t.circle(50)
